f84b438dbdc4b8786a437d3136b553fdbbd82efd
Charles Gunn <cjgunnmail@gmail.com>
2019-09-07 01:48:30 -0400

    hmm x

--- static/present.js
+++ static/present.js
@@ -1,13 +1,48 @@
+last_dec = 1;
+imgs_scaled = false;
+original_img_size = null;
+original_img_marign = null;
+img_size = null;
+img_margin = null;
+
 function add_image(img_url) {
-    if($(".p-body > .p-images").length == 0){
+    if($(".p-body > .p-images").length == 0)
         $(".p-body").append('<div class="p-images"></div>');
+    $(".p-body > .p-images").append("<img src=\"" + img_url + "\" />");
+    num_imgs = $(".p-body > .p-images > img").length;
+    if (num_imgs >= 1.75*last_dec) {
+        last_dec = num_imgs;
+        scale_imgs_down(1.414);
     }
-    $(".p-body > .p-text").append("<img src=\"" + img_url + "\" />");
+    if (img_size != null)
+        correct_imgs();
+}
+
+function scale_imgs_down(factor){
+    if (!imgs_scaled) {
+        original_img_size = $(".p-body > .p-images > img").css("max-width");
+        original_img_marign = $(".p-body > .p-images > img").css("margin");
+    }
+    imgs_scaled = true;
+    width = $(".p-body > .p-images > img").css("max-width");
+    margin = $(".p-body > .p-images > img").css("margin");
+    img_size = "calc(" + width + " / " + factor + ")";
+    img_margin = "calc(" + margin + " / " + factor + ")";
+}
+
+function correct_imgs() {
+    $(".p-body > .p-images > img").css("max-width", img_size)
+                                  .css("max-height", img_size)
+                                  .css("margin", img_margin);
 }
 
 function add_bullet(text) {
     if($(".p-body > .p-text").length == 0){
-        $(".p-body").append('<div class="p-text"><ul></ul></div>');
+        $(".p-body").prepend('<div class="p-text"><ul></ul></div>');
+        if($(".p-body > .p-images").length > 0){
+            scale_imgs_down(0.7);
+            correct_imgs();
+        }
     }
     $(".p-body > .p-text > ul").append("<li>" + text + "</li>");
 }
@@ -15,4 +50,7 @@ function add_bullet(text) {
 function new_slide(title){
     $(".p-body").empty();
     $("#my-p-header h1").html(title);
+    img_size = original_img_size;
+    img_margin = original_img_marign;
+    last_dec = 1;
 }
